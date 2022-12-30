677906a580edacee98d9deffa48f7f41fbb8b6ed
david366 <marinkovic.david366@gmail.com>
2022-12-30 18:53:28 +0100

    Doradjen A5/1 i zavrsen, malo formatiran kod za lepsi prikaz, RSA i dalje ne radi, tj radi, ali problem sa velicinom bloka, ako uzmem premali, desi se da naidjem na situaciju 00 -> 00, i to je ok, ali kad naidjem na situaciju 7210^2399, desava se IntOverflow cak i sa BigInteger klasom

--- ZI_17738/Program.cs
+++ ZI_17738/Program.cs
@@ -5,20 +5,20 @@ namespace ZI_17738
     {
         public static void Main(string[] args)
         {
-            //A5_1 a51 = new A5_1("key12378", 17);
-            //string key = a51.generate_keystream();
-            //Console.WriteLine("Your key: " + key);
+            //A5_1 encoder = new A5_1("daavid12");
+            //byte[] encoded = encoder.encrypt("primer plain teksta podataka:?~");
+
+            //A5_1 decoder = new A5_1("daavid12");
+            //decoder.decrypt(encoded);
 
             //PlayfairCipher pf = new PlayfairCipher("Monarchy");
-            //string test = pf.encrypt("ovo je primer podatakass");
+            //string test = pf.encrypt("primer plain teksta podataka");
             //pf.decrypt(test);
 
             // FIXME: ne radi lepo, overflow za BigInteger...[]
             //RSA rsa = new RSA(10);
             //string res = rsa.encrypt("KARLSRUHE");
             //rsa.decrypt(res);
-
-
 
             Console.ReadLine();
         }
